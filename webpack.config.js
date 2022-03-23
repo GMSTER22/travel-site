25eee5a927c422538df54b5761f3f5f410277050
Gael Souleymane Nongnogo <gmster22@gmail.com>
2022-03-23 18:38:29 -0400

    setup dev server in webpack config to watch html file, installed postcss-mixins that I used to set media queries, styled large hero css, used html tag picture and srset for responsive images in large hero section

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const postCSSPlugins = [
     require("postcss-import"),
+    require("postcss-mixins"),
     require("postcss-simple-vars"),
     require("postcss-nested"),
     require("autoprefixer")
@@ -11,16 +13,34 @@ module.exports = {
     entry: "./app/assets/scripts/App.js",
     output: {
         filename: "bundled.js",
-        path: path.resolve(__dirname, "app")
+        path: path.resolve(__dirname, "app"),
+        clean: true,
     }, 
+    devServer: {
+        watchFiles: ('./app/**/*.html'),
+        static: {
+            directory: path.resolve(__dirname, "app"),
+        },
+        open: true,
+        hot: true,
+        port: 3000,
+        host: "0.0.0.0",
+        // host: "192.168.1.242"
+    },
     mode: "development",
-    watch: true,
+    // watch: true,  (no more need for watch true after installing and using devServer)
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader","css-loader", {loader: "postcss-loader", options: {postcssOptions: {plugins: postCSSPlugins}} }]
+                use: [{loader: "style-loader"},{loader: "css-loader", options: {url: false}}, {loader: "postcss-loader", options: { postcssOptions: {plugins: postCSSPlugins}} }]
             }
         ]
-    }
+    },
+    // plugins: [
+    //     new HtmlWebpackPlugin({
+    //         title: "Webpack App",
+    //         filename: "index.html",
+    //     })
+    // ]
 }
