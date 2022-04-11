6fa65945ae9fae37d73cb99533ce9315dbb97763
Gael Souleymane Nongnogo <gmster22@gmail.com>
2022-04-10 22:37:31 -0400

    completed desktop and mobile header styles

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,10 @@
 import "../styles/styles.css";
+import MobileMenu from "./module/mobileMenu";
 
 if (module.hot) {
     module.hot.accept();
 }
+
+// Lesson example code below this line
+
+let mobileMenu = new MobileMenu();
